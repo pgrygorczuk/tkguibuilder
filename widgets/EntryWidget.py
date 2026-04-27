f6cae8721620607fc569085976b673745d998419
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2026-04-27 21:15:20 +0200

    Entry widget

---
+++ widgets/EntryWidget.py
@@ -0,0 +1,29 @@
+import pygame
+from common import *
+from widgets.Widget import Widget
+
+
+class EntryWidget(Widget):
+	def __init__(self, props:dict={}):
+		super().__init__(props)
+
+	def draw(self, screen:pygame.Surface):
+		super().draw(screen)
+		old_clip = screen.get_clip()
+		screen.set_clip(self.rect)
+		x, y, w, h = self.rect.x, self.rect.y, self.rect.w, self.rect.h
+		font = pygame.font.Font("fonts/"+get_settings("font.family"),
+						  		pt2px(get_settings("font.size")))
+		text = font.render(self.text, True, "black")
+		pygame.draw.rect(screen, "white", self.rect, width=0, border_radius=1)
+		pygame.draw.rect(screen, "dimgray", self.rect, width=1, border_radius=1)
+		rect = text.get_rect(center=(self.rect.centerx, self.rect.centery))		
+		screen.blit(text, [x+2, y])
+		screen.set_clip(old_clip)
+
+	def get_code(self):
+		code = super().get_code()
+		code += f'{self.name} = ttk.Entry(root, text="{self.text}")\n'
+		code += f'{self.name}.place(x={self.rect.x}, y={self.rect.y}, '
+		code += f'width={self.rect.width}, height={self.rect.height})\n'
+		return code
