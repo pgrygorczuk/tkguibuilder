4a2f2bd2c33da738ccea93b518816b42fbf61474
pgrygorczuk <pawelgrygorczuk@poczta.onet.pl>
2026-05-12 10:54:18 +0200

    Checkbutton widget

---
+++ widgets/CheckbuttonWidget.py
@@ -0,0 +1,30 @@
+import pygame
+from common import get_settings, pt2px
+from widgets.Widget import Widget
+
+
+class CheckbuttonWidget(Widget):
+	def __init__(self, props:dict={}):
+		super().__init__(props)
+
+	def draw(self, screen:pygame.Surface):
+		super().draw(screen)
+		old_clip = screen.get_clip()
+		screen.set_clip(self.rect)
+		x, y, w, h = self.rect.x, self.rect.centery - 6, 12, 12
+		font = pygame.font.Font("fonts/"+get_settings("font.family"),
+						  		pt2px(get_settings("font.size")))
+		text = font.render(self.text, True, "black")
+		pygame.draw.rect(screen, "black", pygame.Rect(x, y, w, h), width=1, border_radius=1)
+		rect = text.get_rect(center=(self.rect.centerx, self.rect.centery))		
+		screen.blit(text, [ x+1.5*w, y-6 ])
+		screen.set_clip(old_clip)
+
+	def get_code(self, indent:int=0):
+		code = super().get_code(indent)
+		ind = "\t"*indent
+		return code + (
+			f'{ind}{self.vname} = ttk.Checkbutton(self, font=self.font, text="{self.text}", takefocus=0)\n'
+			f'{ind}{self.vname}.place(x={self.rect.x}, y={self.rect.y}, '
+			f'width={self.rect.width}, height={self.rect.height})\n'
+		)
